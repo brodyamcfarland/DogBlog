62b725acd3fce240bed83826eefd495062874f2e
brodyamcfarland <brodyamcfarland@gmail.com>
2022-08-31 16:56:14 -0700

    Added Pagination and Logic

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -24,7 +24,7 @@ export const signup = async (req, res) => {
         if(password !== confirmPassword) return res.status(404).json({ message: "Password does not match." });
         const hashedPassword = await bcrypt.hash(password, 12);
         const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
-        const token = jwt.sign({ email: result.email, id: result._id }, 'test', { expiresIn: '1h'});
+        const token = jwt.sign({ email: result.email, id: result._id}, 'test', { expiresIn: '1h'});
         res.status(200).json({ result: result, token })
     } catch (error) {
         res.status(500).json({ message: 'Something went wrong.'})
