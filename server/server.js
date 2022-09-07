7bc9666716c1c2a27b3a5777616ff63403870250
brodyamcfarland <brodyamcfarland@gmail.com>
2022-09-07 07:17:56 -0700

    First Commit

--- server/server.js
+++ server/server.js
@@ -13,6 +13,10 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('Server is running successfully.');
+});
+
 //localhost:5000/posts/
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
